339afb8220b5b4530c92ce8d59b8e6d1ecb807bb
ilmar521 <118733735+ilmar521@users.noreply.github.com>
2023-06-29 18:56:11 +0300

    fix some trouble

--- main.js
+++ main.js
@@ -22,17 +22,3 @@ $('#posts').on('click', '.button_comment', function () {
     tweeter.addComment($(this).siblings('.input_comment').val(), $(this).closest('.post').data('id'))
     renderer.renderPosts(tweeter.getPosts())   
 })
-
-
-{/* <div class="post" data-id="p1">
-<p class="post-text">First post!</p>
-<div class="comments">
-  <p data-id="c1">
-    <button class="delete-comment">X</button> First comment on first post!
-  </p>
-  <p data-id="c2"><button class="delete-comment">X</button> Second comment on first post!!</p>
-  <p data-id="c3"><button class="delete-comment">X</button> Third comment on first post!!!</p>
-  <input class="input_comment" type="text" placeholder="Got something to say?">
-  <button class="button_comment">Comment</button>
-</div><button class="delete">Delete post</button>
-</div> */}
